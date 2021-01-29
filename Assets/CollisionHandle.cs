e756025c337e1d54515ff3df4fbeb220015c3130
lemadmax <xzhengrui@gmail.com>
2021-01-29 17:10:26 -0500

    obstacle added

---
+++ Assets/CollisionHandle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandle : MonoBehaviour
+{
+
+    public PlayerMovement playerMovement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            playerMovement.enabled = false;
+        }
+    }
+
+}
